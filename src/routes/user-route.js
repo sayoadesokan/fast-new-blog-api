3a340a2c91a777d0bbbafd4f23d16f9fdeebe3b1
jamesnottidge <freshice57@gmail.com>
2023-10-18 15:08:36 +0100

    route validation middleware

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,17 +1,37 @@
-const express = require('express');
+const express = require("express");
+import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "../middleware/middleware";
 const {
   signUp,
   createUser,
   followUser,
-} = require('../controller/user-controller');
-const { Authenticate } = require('../middleware/auth');
+} = require("../controller/user-controller");
+const { Authenticate } = require("../middleware/auth");
 
 const route = express.Router();
 
 route
-  .post('/register', createUser)
-  .post('/login', signUp)
+  .post(
+    "/register",
+    body("email").exists().trim().isEmail(),
+    body("password").exists().isString().trim().notEmpty(),
+    body("firstName").exists().isString().trim().notEmpty(),
+    handleInputErrors,
+    createUser
+  )
+  .post(
+    "/login",
+    body("email").exists().trim().isEmail(),
+    body("password").exists().isString().trim().notEmpty(),
+    handleInputErrors,
+    signUp
+  )
   .use(Authenticate)
-  .post('/follow', followUser);
+  .post(
+    "/follow",
+    body("firstName").exists().isString().trim().notEmpty(),
+    handleInputErrors,
+    followUser
+  );
 
 module.exports = route;
--- src/utils/express-app.js
+++ src/utils/express-app.js
@@ -25,7 +25,7 @@ module.exports = async (app) => {
   });
   app.use(apiRequestLimiter);
 
-  app.use('/v1/user', userRouter);
+  app.use('/v1/user',  userRouter);
   app.use('/v1/contact', contactRouter);
   app.use('/v1/blog', blogRouter);
 };
